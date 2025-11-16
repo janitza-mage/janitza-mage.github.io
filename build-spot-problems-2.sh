1279f346c84615196369f39c509d3c9d89db918e
Martin Geisse <martin.geisse@janitza.de>
2025-11-16 14:37:37 +0100

    ...

--- build-spot-problems-2.sh
+++ build-spot-problems-2.sh
@@ -3,4 +3,5 @@ rm -rf build
 npm run build
 cd ../janitza-mage.github.io/spot-problems-2
 rm -rf *
-cp -R ../spot-problems-2/build/* .
+cp -R ../../spot-problems-2/docs/* .
+cp -R ../../spot-problems-2/public/index.html .
